dd7285130062cbc886784b5afb60db4bf54361f8
Wes <we_cheung@yahoo.com>
2019-06-06 23:19:57 -0400

    added back missing init function

--- ptsemseg/models/segnet_mcdo.py
+++ ptsemseg/models/segnet_mcdo.py
@@ -135,6 +135,51 @@ class segnet_mcdo(nn.Module):
         self.softmaxMCDO = torch.nn.Softmax(dim=1)
         self.softmax = torch.nn.Softmax(dim=0)
 
+    def init_vgg16_params(self, vgg16):
+        blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
+
+        ranges = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 29]]
+        features = list(vgg16.features.children())
+
+        vgg_layers = []
+        for _layer in features:
+            if isinstance(_layer, nn.Conv2d):
+                vgg_layers.append(_layer)
+
+        merged_layers = []
+        for idx, conv_block in enumerate(blocks):
+            if idx < 2:
+                units = [conv_block.conv1.cbr_unit, conv_block.conv2.cbr_unit]
+            else:
+                units = [
+                    conv_block.conv1.cbr_unit,
+                    conv_block.conv2.cbr_unit,
+                    conv_block.conv3.cbr_unit,
+                ]
+            for _unit in units:
+                for _layer in _unit:
+                    if isinstance(_layer, nn.Conv2d):
+                        merged_layers.append(_layer)
+
+        assert len(vgg_layers) == len(merged_layers)
+
+        for l1, l2 in zip(vgg_layers, merged_layers):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                if l1.weight.size() == l2.weight.size() and l1.bias.size() == l2.bias.size():
+                    assert l1.weight.size() == l2.weight.size()
+                    assert l1.bias.size() == l2.bias.size()
+                    l2.weight.data = l1.weight.data
+                    l2.bias.data = l1.bias.data
+                else:
+
+                    num_orig = int(l1.weight.size()[1])
+                    num_tiles = int(l2.weight.size()[1]) // int(l1.weight.size()[1])
+
+                    for i in range(num_tiles):
+                        l2.weight.data[:, i * num_orig:(i + 1) * num_orig, :, :] = l1.weight.data
+
+        l2.bias.data = l1.bias.data
+
     def forwardOnce(self, inputs, pass_no):
 
         # Use MCDO if Multiple Passes

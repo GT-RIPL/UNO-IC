fixed bug with analysis taking averages and added random seeding option

analysis.py
@@ -168,15 +168,11 @@ data = []
 # exit()
 
 for run in runs.keys():
-    #print(run)
 
-
+    print(run)
     conditions = runs[run]['std_config']
 
     name = ", ".join(["{}{}".format(k,v) for k,v in conditions.items()])
-
-
-
 
     for full in [k for k in runs[run].keys() if "config" not in k]:
         #print(full)
@@ -221,9 +217,13 @@ for run in runs.keys():
 
         # RESULTS
         # avg + std of last 50k iterations
-        i = -1 #x.index(int(500*(x[-1] // 500))-5000)
-        avg = np.mean(y)
-        std = np.std(y)
+        i = -1  #x.index(int(500*(x[-1] // 500))-5000)
+        avg = np.mean(y[i:])
+        std = np.std(y[i:])
+
+        avg = y[-1]
+        std = 0
+
 
         test_pretty = filter(None,test.split("_"))
         test_pretty = [s for s in test_pretty if s not in ["8camera","dense"]]
@@ -237,10 +237,7 @@ for run in runs.keys():
                         "test":test_pretty,
                         "mean":avg,
                         "std":std},
-                        "iter":x[-1]})
-
-
-
+                        "iter":x[i]})
 
 
 df = pd.DataFrame(data)
@@ -258,15 +255,6 @@ df = df[data_fields]
 df['unique_id'] = (df.groupby(id_fields).cumcount()) 
 df['full'] = df['pretty']
 df = df.sort_values(by=id_fields)
-
-df = df[ 
-        (
-            (df['size'] != "")
-        ) | ( 
-            (df['size'] == "")
-
-        )]
-
 
 df = df.set_index(["test"])
 df = df.pivot_table(index=df.index, values='mean', columns='full', aggfunc='first')

train.py
@@ -1,5 +1,5 @@
-
 import matplotlib
+
 matplotlib.use('Agg')
 
 import os
@@ -28,11 +28,10 @@ from tensorboardX import SummaryWriter
 
 from functools import partial
 from collections import defaultdict
-
+import time
 # SWAG lib imports
 from ptsemseg.posteriors import SWAG
 from ptsemseg.utils import bn_update, mem_report
-
 
 
 def train(cfg, writer, logger, logdir):
@@ -103,15 +102,14 @@ def train(cfg, writer, logger, logdir):
 
         loss_fn = get_loss_function(cfg)
         logger.info("Using loss {}".format(loss_fn))
-        
-        
+
         # setup swa training
         if cfg['swa']:
             print('SWAG training')
             swag_models[model] = SWAG(models[model],
-                              no_cov_mat=False,
-                              max_num_models=20)
-            
+                                      no_cov_mat=False,
+                                      max_num_models=20)
+
             swag_models[model].to(device)
         else:
             print('SGD training')
@@ -164,20 +162,19 @@ def train(cfg, writer, logger, logdir):
                 print("No checkpoint found at '{}'".format(model_pkl))
                 exit()
 
-
     best_iou = -100.0
     i = start_iter
     print("Beginning Training at iteration: {}".format(i))
     while i < cfg["training"]["train_iters"]:
-    
+
         #################################################################################
         # Training
         #################################################################################
         print("=" * 10, "TRAINING", "=" * 10)
         for (input_list, labels_list) in loaders['train']:
-            
+
             inputs, labels = parseEightCameras(input_list['rgb'], labels_list, input_list['d'], device)
-            
+
             # Read batch from only one camera
             bs = cfg['training']['batch_size']
             images = {m: inputs[m][:bs, :, :, :] for m in cfg["models"].keys()}
@@ -216,16 +213,16 @@ def train(cfg, writer, logger, logdir):
                     logger.info(print_str)
                     writer.add_scalar('loss/train_loss/' + m, loss[m].item(), i + 1)
                 time_meter.reset()
-                
+
             # collect parameters for swa
             if cfg['swa'] and (i + 1 - cfg['swa']['start']) % cfg['swa']['c_iterations'] == 0:
-                print('Saving SWA model at iteration: ', i+1)
+                print('Saving SWA model at iteration: ', i + 1)
                 swag_models[m].collect_model(models[m])
-            
+
             if (i + 1) % cfg["training"]["val_interval"] == 0 or (i + 1) >= cfg["training"]["train_iters"]:
 
                 [models[m].eval() for m in models.keys()]
-                
+
                 if cfg['swa']:
                     print('Updating SWA model')
                     swag_models[m].sample(0.0)
@@ -243,12 +240,13 @@ def train(cfg, writer, logger, logdir):
                         output_all = torch.zeros(
                             (len(loaders['recal']) * bs, n_classes, cfg['data']['img_rows'], cfg['data']['img_cols']))
                         labels_all = torch.zeros(
-                            (len(loaders['recal']) * bs, cfg['data']['img_rows'], cfg['data']['img_cols']), dtype=torch.long)
+                            (len(loaders['recal']) * bs, cfg['data']['img_rows'], cfg['data']['img_cols']),
+                            dtype=torch.long)
 
                         with torch.no_grad():
                             for i_recal, (input_list, labels_list) in tqdm(enumerate(loaders['recal'])):
-                                
-                                inputs, labels = parseEightCameras(input_list['rgb'], labels_list, input_list['d'], device)
+                                inputs, labels = parseEightCameras(input_list['rgb'], labels_list, input_list['d'],
+                                                                   device)
 
                                 # Read batch from only one camera
                                 images_recal = inputs[m][:bs, :, :, :]
@@ -276,12 +274,13 @@ def train(cfg, writer, logger, logdir):
                         for i_val, (input_list, labels_list) in tqdm(enumerate(valloader)):
 
                             inputs, labels = parseEightCameras(input_list['rgb'], labels_list, input_list['d'], device)
-                            inputs_display, _ = parseEightCameras(input_list['rgb_display'], labels_list, input_list['d_display'], device)
-                            
+                            inputs_display, _ = parseEightCameras(input_list['rgb_display'], labels_list,
+                                                                  input_list['d_display'], device)
+
                             # import ipdb; ipdb.set_trace() # BREAKPOINT
                             # Read batch from only one camera
                             bs = cfg['training']['batch_size']
-                            
+
                             images_val = {m: inputs[m][:bs, :, :, :] for m in cfg["models"].keys()}
                             labels_val = labels[:bs, :, :]
 
@@ -331,7 +330,8 @@ def train(cfg, writer, logger, logdir):
                             if i_val % cfg["training"]["png_frames"] == 0:
                                 plotPrediction(logdir, cfg, n_classes, i + 1, i_val, k, inputs_display, pred, gt)
                                 for m in cfg["models"].keys():
-                                    plotMeansVariances(logdir, cfg, n_classes, i + 1, i_val, m, k + "/" + m, inputs_display,
+                                    plotMeansVariances(logdir, cfg, n_classes, i + 1, i_val, m, k + "/" + m,
+                                                       inputs_display,
                                                        pred, gt, mean[m], variance[m])
 
                             running_metrics_val[k].update(gt, pred)
@@ -397,10 +397,9 @@ def train(cfg, writer, logger, logdir):
                             torch.save(state, save_path)
 
             i += 1
-            
+
             if i >= cfg["training"]["train_iters"]:
                 break
-
 
 
 if __name__ == "__main__":
@@ -422,9 +421,20 @@ if __name__ == "__main__":
     logdir = "/".join(["runs"] + args.config.split("/")[1:])
     writer = SummaryWriter(logdir)
 
-    print("RUNDIR: {}".format(logdir))
-    shutil.copy(args.config, logdir)
+    path = shutil.copy(args.config, logdir)
 
+    # generate seed if none present
+    if cfg['seed'] is None:
+        seed = int(time.time())
+        cfg['seed'] = seed
+
+        # modify file to reflect seed
+        with open(path, 'r') as original:
+            data = original.read()
+        with open(path, 'w') as modified:
+            modified.write("seed: {}\n".format(seed) + data)
+
+    print("RUNDIR: {}".format(logdir))
     logger = get_logger(logdir)
 
     # baseline train (concatenation, warping baselines)

validate.py
@@ -17,7 +17,7 @@ import cv2
 from ptsemseg.models import get_model
 from ptsemseg.loss import get_loss_function
 from ptsemseg.loader import get_loaders
-from ptsemseg.utils import get_logger, parseEightCameras, plotPrediction, plotMeansVariances, plotEntropy, plotMutualInfo
+from ptsemseg.utils import get_logger, parseEightCameras, plotPrediction, plotMeansVariances#, plotEntropy, plotMutualInfo
 from ptsemseg.metrics import runningScore, averageMeter
 from ptsemseg.schedulers import get_scheduler
 from ptsemseg.optimizers import get_optimizer
@@ -47,13 +47,13 @@ def train(cfg, writer, logger, logdir):
     running_metrics_val = {env: runningScore(n_classes) for env in loaders['val'].keys()}
 
     # Setup Meters
-    #val_loss_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
-    #val_CE_loss_meter = {env: averageMeter() for env in loaders['val'].keys()}
-    #val_REG_loss_meter = {env: averageMeter() for env in loaders['val'].keys()}
-    #variance_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
-    #entropy_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
-    #mutual_info_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
-    #time_meter = averageMeter()
+    val_loss_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
+    val_CE_loss_meter = {env: averageMeter() for env in loaders['val'].keys()}
+    val_REG_loss_meter = {env: averageMeter() for env in loaders['val'].keys()}
+    variance_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
+    entropy_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
+    mutual_info_meter = {m: {env: averageMeter() for env in loaders['val'].keys()} for m in cfg["models"].keys()}
+    time_meter = averageMeter()
 
     start_iter = 0
     models = {}
@@ -89,17 +89,17 @@ def train(cfg, writer, logger, logdir):
         models[model] = torch.nn.DataParallel(models[model], device_ids=range(torch.cuda.device_count()))
 
         # Setup optimizer, lr_scheduler and loss function
-        #optimizer_cls = get_optimizer(cfg)
-        #optimizer_params = {k: v for k, v in cfg['training']['optimizer'].items()
-        #                    if k != 'name'}
+        optimizer_cls = get_optimizer(cfg)
+        optimizer_params = {k: v for k, v in cfg['training']['optimizer'].items()
+                           if k != 'name'}
 
-        #optimizers[model] = optimizer_cls(models[model].parameters(), **optimizer_params)
-        #logger.info("Using optimizer {}".format(optimizers[model]))
+        optimizers[model] = optimizer_cls(models[model].parameters(), **optimizer_params)
+        logger.info("Using optimizer {}".format(optimizers[model]))
 
-        #schedulers[model] = get_scheduler(optimizers[model], cfg['training']['lr_schedule'])
+        schedulers[model] = get_scheduler(optimizers[model], cfg['training']['lr_schedule'])
 
-        #loss_fn = get_loss_function(cfg)
-        #logger.info("Using loss {}".format(loss_fn))
+        loss_fn = get_loss_function(cfg)
+        logger.info("Using loss {}".format(loss_fn))
 
         # Load pretrained weights
         if str(attr['resume']) is not "None" and not "caffemodel" in attr['resume']:
@@ -187,9 +187,10 @@ def train(cfg, writer, logger, logdir):
 
     with torch.no_grad():
         for k, valloader in loaders['val'].items():
-            for i_val, (images_list, labels_list, aux_list) in tqdm(enumerate(valloader)):
+            for i_val, (input_list, labels_list) in tqdm(enumerate(valloader)):
 
-                inputs, labels = parseEightCameras(images_list, labels_list, aux_list, device)
+                inputs, labels = parseEightCameras(input_list['rgb'], labels_list, input_list['d'], device)
+                inputs_display, _ = parseEightCameras(input_list['rgb_display'], labels_list, input_list['d_display'], device)
 
                 # Read batch from only one camera
                 bs = cfg['training']['batch_size']
@@ -204,6 +205,7 @@ def train(cfg, writer, logger, logdir):
                 variance = {}
                 entropy = {}
                 mutual_info = {}
+                val_loss = {}
 
                 for m in cfg["models"].keys():
                     if hasattr(models[m].module, 'forwardMCDO'):
@@ -212,7 +214,7 @@ def train(cfg, writer, logger, logdir):
                     else:
                         mean[m] = models[m](images_val[m])
                         variance[m] = torch.zeros(mean[m].shape)
-                    #val_loss[m] = loss_fn(input=mean[m], target=labels_val)
+                    val_loss[m] = loss_fn(input=mean[m], target=labels_val)
 
                 #import ipdb;ipdb.set_trace()
                 # Fusion Type
@@ -246,15 +248,34 @@ def train(cfg, writer, logger, logdir):
                 gt = labels_val.data.cpu().numpy()
                 #import ipdb;ipdb.set_trace()
                 if i_val % cfg["training"]["png_frames"] == 0:
-                    plotPrediction(logdir, cfg, n_classes, i, i_val, k, inputs, pred, gt)
+                    plotPrediction(logdir, cfg, n_classes, i, i_val, k, inputs_display, pred, gt)
                     for m in cfg["models"].keys():
-                        plotMeansVariances(logdir, cfg, n_classes, i + 1, i_val, m, k + "/" + m, inputs,
+                        plotMeansVariances(logdir, cfg, n_classes, i + 1, i_val, m, k + "/" + m, inputs_display,
                                            pred, gt, mean[m], variance[m])
-                        plotEntropy(logdir, i, i_val, k + "/" + m, pred, entropy[m])
-                        plotMutualInfo(logdir, i, i_val, k + "/" + m, pred, mutual_info[m])
+                        # plotEntropy(logdir, i, i_val, k + "/" + m, pred, entropy[m])
+                        # plotMutualInfo(logdir, i, i_val, k + "/" + m, pred, mutual_info[m])
                         
-
+                running_metrics_val[k].update(gt, pred)
             
+        for m in cfg["models"].keys():
+            for k in loaders['val'].keys():
+                writer.add_scalar('loss/val_loss/{}/{}'.format(m, k), val_loss_meter[m][k].avg, i + 1)
+                logger.info("%s %s Iter %d Loss: %.4f" % (m, k, i + 1, val_loss_meter[m][k].avg))
+
+    for env, valloader in loaders['val'].items():
+        score, class_iou = running_metrics_val[env].get_scores()
+        for k, v in score.items():
+            logger.info('{}: {}'.format(k, v))
+            writer.add_scalar('val_metrics/{}/{}'.format(env, k), v, i + 1)
+
+        for k, v in class_iou.items():
+            logger.info('{}: {}'.format(k, v))
+            writer.add_scalar('val_metrics/{}/cls_{}'.format(env, k), v, i + 1)
+
+        for m in cfg["models"].keys():
+            val_loss_meter[m][env].reset()
+        running_metrics_val[env].reset()
+
 
 
 
fixed prob plotting and multiple inputs to uncertainty scaling

ptsemseg/models/fusion/CAFnet.py
@@ -92,8 +92,8 @@ class CAFnet(nn.Module):
             param.requires_grad = False
 
         self.fusion = self._get_fusion_module(fusion_module, n_classes)
-        # self.scale = self._get_scale_module(scaling_module, rgb_init=self.rgb_segnet.module.temperature, d_init=self.d_segnet.module.temperature)
-        self.scale = self._get_scale_module(scaling_module)
+        # self.scale = self._get_scale_module(scaling_module, 2, rgb_init=self.rgb_segnet.module.temperature, d_init=self.d_segnet.module.temperature)
+        self.scale = self._get_scale_module(scaling_module, 2)
         self.i = 0
 
         self.fuseProbabilities = True
@@ -197,12 +197,12 @@ class CAFnet(nn.Module):
             "NoisyOr": NoisyOr(n_classes),
         }[name]
 
-    def _get_scale_module(self, name, rgb_init=None, d_init=None):
+    def _get_scale_module(self, name,n_filters=2, rgb_init=None, d_init=None):
 
         name = str(name)
 
         return {
             "temperature": TemperatureScaling(rgb_init=rgb_init, d_init=d_init),
-            "uncertainty": UncertaintyScaling(rgb_init=rgb_init, d_init=d_init),
+            "uncertainty": UncertaintyScaling(n_filters, rgb_init=rgb_init, d_init=d_init),
             "None": None
         }[name]

ptsemseg/models/fusion/fusion.py
@@ -141,16 +141,16 @@ class TemperatureScaling(nn.Module):
 
 # 1.0
 class UncertaintyScaling(nn.Module):
-    def __init__(self, rgb_init=None, d_init=None):
+    def __init__(self, n_filters, rgb_init=None, d_init=None):
         super(UncertaintyScaling, self).__init__()
-        self.rgb_scale = nn.Conv2d(1,
+        self.rgb_scale = nn.Conv2d(n_filters,
                                    1,
                                    3,
                                    stride=1,
                                    padding=1,
                                    bias=True,
                                    dilation=1)
-        self.d_scale = nn.Conv2d(1,
+        self.d_scale = nn.Conv2d(n_filters,
                                  1,
                                  3,
                                  stride=1,
@@ -159,9 +159,9 @@ class UncertaintyScaling(nn.Module):
                                  dilation=1)
         
         
-        self.rgb_scale.weight = torch.nn.Parameter(torch.zeros((1,1,3,3)))
+        self.rgb_scale.weight = torch.nn.Parameter(torch.zeros((1,n_filters,3,3)))
         
-        self.d_scale.weight = torch.nn.Parameter(torch.zeros((1,1,3,3)))
+        self.d_scale.weight = torch.nn.Parameter(torch.zeros((1,n_filters,3,3)))
           
         if rgb_init is not None:
             self.rgb_scale.bias = torch.nn.Parameter(rgb_init)
@@ -172,6 +172,7 @@ class UncertaintyScaling(nn.Module):
         else:
             self.d_scale.bias = torch.nn.Parameter(torch.ones(1))
         
+        self.n_filters = n_filters
         
 
     def forward(self, mean, variance, mutual_info, entropy):
@@ -179,10 +180,13 @@ class UncertaintyScaling(nn.Module):
         rgb, rgb_var, rgb_mi, rgb_entropy = mean['rgb'], variance['rgb'], mutual_info['rgb'], entropy['rgb']
         d, d_var, d_mi, d_entropy = mean['d'], variance['d'], mutual_info['d'], entropy['d']
         
-        # rgb_s = self.rgb_scale(torch.cat([rgb_var, rgb_entropy.unsqueeze(1)], dim=1))
-        # d_s = self.d_scale(torch.cat([d_var, d_entropy.unsqueeze(1)], dim=1))
-        rgb_s = self.rgb_scale(rgb_var)
-        d_s = self.d_scale(d_var)
+        if self.n_filters == 2:
+            rgb_s = self.rgb_scale(torch.cat([rgb_var, rgb_entropy.unsqueeze(1)], dim=1))
+            d_s = self.d_scale(torch.cat([d_var, d_entropy.unsqueeze(1)], dim=1))
+        else:
+            rgb_s = self.rgb_scale(rgb_var)
+            d_s = self.d_scale(d_var)
+
         print("rgb weight: {}".format(self.rgb_scale.weight.mean()))
         print("d weight: {}".format(self.d_scale.weight.mean()))
         print("rgb bias: {}".format(self.rgb_scale.bias))